9692b25dbd4c0a3e9f56a21399198dba36dcccf9
nani-stark-3000 <naninarendra1506@gmail.com>
2022-11-22 19:47:59 +0530

    working

---
+++ flaskmain.py
@@ -0,0 +1,37 @@
+from flask import Flask,render_template,request,make_response
+from openpyxl import load_workbook
+app = Flask(__name__,template_folder='template')
+
+@app.route('/nn', methods=['POST','GET'])
+
+def index_home():
+    if request.method=='POST':
+        link = request.form['file']
+        link = link.replace('"','')
+        wb = load_workbook(link)
+        sheet = wb.active
+        day = request.form['day']
+        print(day)
+        index = chr(int(day)+67)
+        head = index+'1'
+        sheet[head]='Day-'+str(day)
+        nlist = request.form['nlist'].split()
+        llist = request.form['llist'].split()
+
+        n= ['21L31A54'+i for i in nlist]
+        l= ['22L35A54'+i for i in llist]
+
+        for i in range(2,74):
+            roll='B'+str(i)
+            atnd=index+str(i)
+            if (sheet[roll].value in n) or (sheet[roll].value in l):
+                sheet[atnd]= 0
+            else:
+                sheet[atnd]= 1 
+        wb.save(link)
+        return render_template('new.html',sheet=sheet)
+    return render_template('new.html')
+    
+
+if __name__ == "__main__":
+    app.run(debug=True,port=47)
--- main.py
+++ main.py
@@ -4,6 +4,10 @@ wb = load_workbook(filename="c++ attendance.xlsx")
 
 sheet = wb.active
 
+day = int(input("Enter day \"ex : if day-1 then enter 1\": "))
+index = chr(day+67)
+head = index+'1'
+sheet[head]='Day-'+str(day)
 nlist = input("Enter N Absenties : ").split()
 llist = input("Enter L Absenties : ").split()
 
@@ -12,12 +16,10 @@ l= ['22L35A54'+i for i in llist]
 
 for i in range(2,74):
     roll='B'+str(i)
-    atnd='G'+str(i)
+    atnd=index+str(i)
     if (sheet[roll].value in n) or (sheet[roll].value in l):
         sheet[atnd]= 0
     else:
         sheet[atnd]= 1 
-
-
-    
-wb.save(filename="c++ attendance.xlsx")
+        
+wb.save(filename="sample.xlsx")
